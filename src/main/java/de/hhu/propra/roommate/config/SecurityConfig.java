c62da80a59f0827b9d8d77083cbddbba8413bfde
Guga Glonti <110097877+GugaGlonti@users.noreply.github.com>
2023-11-20 14:42:57 +0400

    add github OAuth Authentication

--- src/main/java/de/hhu/propra/roommate/config/SecurityConfig.java
+++ src/main/java/de/hhu/propra/roommate/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package de.hhu.propra.roommate.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -11,11 +12,13 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity chainBuilder) throws Exception {
-        chainBuilder.authorizeHttpRequests(
-                configurer -> configurer.requestMatchers("/","/css/*").permitAll()
-                        .anyRequest().authenticated()
-        );
-        return chainBuilder.build();
+        return chainBuilder.authorizeHttpRequests(
+                configurer -> configurer
+                        .requestMatchers("/", "/css/*").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+                .oauth2Login(Customizer.withDefaults())
+                .build();
     }
 
 }
